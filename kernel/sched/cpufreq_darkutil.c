43fb973d4c6e33e087768cf4e94c75f968ab977c
Ahmad Thoriq Najahi <najahiii@outlook.co.id>
2019-08-02 18:40:38 +0000

    sched: darkutil: cleanup WALT for PELT
    Signed-off-by: Ahmad Thoriq Najahi <najahiii@outlook.co.id>

--- kernel/sched/cpufreq_darkutil.c
+++ kernel/sched/cpufreq_darkutil.c
@@ -276,10 +276,7 @@ static void dugov_get_util(unsigned long *util, unsigned long *max, u64 time)
 	rt = (rt * max_cap) >> SCHED_CAPACITY_SHIFT;
 
 	*util = min(rq->cfs.avg.util_avg + rt, max_cap);
-#ifdef CONFIG_SCHED_WALT
-	if (!walt_disabled && sysctl_sched_use_walt_cpu_util)
-		*util = boosted_cpu_util(cpu);
-#endif
+
 	*max = max_cap;
 }
 
